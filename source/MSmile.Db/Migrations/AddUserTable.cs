f6563e7b46c30d9dfc64e2fd6398013d92bff5a7
Mikhail Rozum <v0id.nvkz@gmail.com>
2020-09-19 23:36:56 +0700

    + Users

---
+++ source/MSmile.Db/Migrations/AddUserTable.cs
@@ -0,0 +1,29 @@
+﻿namespace MSmile.Db.Migrations
+{
+    using FluentMigrator;
+
+    /// <summary>
+    /// Creates User table
+    /// </summary>
+    [Migration(202009192331, "Creates User table")]
+    public class AddUserTable : Migration
+    {
+        private const string TableName = "User";
+
+        /// <inheritdoc />
+        public override void Up()
+        {
+            this.Create.Table(TableName).WithDescription("Users")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Login").AsString(50).NotNullable()
+                .WithColumn("PasswordHash").AsString(500).NotNullable()
+                .WithColumn("EmployeeId").AsInt64().ForeignKey("Employee", "Id");
+        }
+
+        /// <inheritdoc />
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
