ec97d4e255d147ea3d1f60d6be9ecc8ee95f3419
Mikhail Rozum <v0id.nvkz@gmail.com>
2020-09-26 14:07:34 +0700

    Added logging

--- source/MSmile.Api/Program.cs
+++ source/MSmile.Api/Program.cs
@@ -2,6 +2,7 @@ namespace MSmile.Api
 {
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
 
     public class Program
     {
@@ -12,6 +13,13 @@ namespace MSmile.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(
+                    (context, builder) =>
+                    {
+                        builder.ClearProviders();
+                        builder.AddConsole();
+                        builder.AddFile("Logs\\Log-{Date}.log");
+                    })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
